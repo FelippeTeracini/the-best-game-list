2801c70e894b05f7e1df1ab787151ef11bfb5744
Terabyte900 <fe.n.teracini@gmail.com>
2024-04-15 19:33:07 -0300

    Changed NavBar Colors

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,6 +1,6 @@
 import { Button } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { blue, grey } from '@mui/material/colors';
+import { indigo, blueGrey } from '@mui/material/colors';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -10,23 +10,23 @@ import { Link } from 'react-router-dom';
 
 const theme = createTheme({
   palette: {
-    primary: blue,
-    secondary: grey,
+    primary: indigo,
+    secondary: blueGrey,
   },
 });
 
 const NavBar = () => {
   return (
     <Box sx={{ flexGrow: 1}}>
-      <AppBar position="static" color='primary'>
+      <AppBar position="static" style={{backgroundColor:'black'}}>
         <Toolbar>
           <Typography variant="h5" style={{textDecoration:"none", color:'white'}} component={Link} to="/" sx={{ flexGrow: 1 }}>
             TheBestGameList
           </Typography>
           <Typography sx={{ flexGrow: 8 }}></Typography>
           <ThemeProvider theme={theme}>
-            <Button variant="contained" color='secondary' sx={{ flexGrow: 1, mr:2}} component={Link} to="/signUp"><b>Sign Up</b></Button>
-            <Button variant="contained" color='primary' sx={{ flexGrow: 1}} component={Link} to="/login"><b>Login</b></Button>
+            <Button variant="contained" color='secondary' sx={{ flexGrow: 1, mr:2}} component={Link} to="/login"><b>Login</b></Button>
+            <Button variant="contained" color='primary' sx={{ flexGrow: 1}} component={Link} to="/signUp"><b>Sign Up</b></Button>
           </ThemeProvider>
         </Toolbar>
       </AppBar>
